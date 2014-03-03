115cd0dd30bb1c0e9fc12961661f97bd311f91f7
Karin <karin.j.alvarez@gmail.com>
2014-03-03 14:55:47 -0700

    Working on splice

--- src.js
+++ src.js
@@ -10,10 +10,11 @@
 var splice = function(arr, start, numToReplace, replace) {
 	newArray= [];
 	for (i=0; i<arr.length; i++) {
-		if (arr[i] <= arr[start] || 
-			arr[i] > arr[start + numToReplace]) {
+		if (arr[i] < arr[start] || 
+			arr[i] >= arr[start + numToReplace]) {
 			newArray.push(arr[i]);
-		} 
+		};
+
 	} 
 		return newArray;
 };
